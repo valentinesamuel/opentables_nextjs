bc0d09cc6ce4bd157eff753cf67fd5a73fab878f
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-10 15:29:34 +0100

    FEAT: Added searching by params

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,12 +1,39 @@
 /* eslint-disable react/no-unescaped-entities */
 import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
-import { PrismaClient } from "@prisma/client";
-import { usePathname } from "next/navigation";
+import { PRICE, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+interface SearchParams {
+  city?: string;
+  cuisine?: string;
+  price?: PRICE;
+}
+const fetchRestaurantsByCity = (searchParams: SearchParams) => {
+  const where: any = {};
 
-const fetchRestaurantsByCity = (city: string) => {
+  if (searchParams.city) {
+    const location = {
+      name: {
+        equals: searchParams.city.toLowerCase(),
+      },
+    };
+    where.location = location;
+  }
+  if (searchParams.cuisine) {
+    const cuisine = {
+      name: {
+        equals: searchParams.cuisine.toLowerCase(),
+      },
+    };
+    where.cuisine = cuisine;
+  }
+  if (searchParams.price) {
+    const price = {
+      equals: searchParams.price,
+    };
+    where.price = price;
+  }
   const select = {
     id: true,
     name: true,
@@ -16,24 +43,32 @@ const fetchRestaurantsByCity = (city: string) => {
     location: true,
     slug: true,
   };
-  if (!city) return prisma.restaurant.findMany({ select });
   return prisma.restaurant.findMany({
-    where: {
-      location: {
-        name: {
-          equals: city.toLowerCase(),
-        },
-      },
-    },
+    where,
     select,
   });
 };
-const Search = async ({ searchParams }: { searchParams: { city: string } }) => {
-  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+
+const fetchLocations = async () => {
+  const locations = prisma.location.findMany();
+  return locations;
+};
+const fetchCuisines = async () => {
+  const cuisines = prisma.cuisine.findMany();
+  return cuisines;
+};
+const Search = async ({ searchParams }: { searchParams: SearchParams }) => {
+  const restaurants = await fetchRestaurantsByCity(searchParams);
+  const locations = await fetchLocations();
+  const cuisines = await fetchCuisines();
 
   return (
     <>
-      <SearchSideBar />
+      <SearchSideBar
+        locations={locations}
+        cuisines={cuisines}
+        searchParams={searchParams}
+      />
       <div className="w-5/6">
         {restaurants.length >= 1
           ? restaurants.map((restaurant) => {
