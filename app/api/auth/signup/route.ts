7a2fc1bf895b62ae7467ad5588269ddeec5c0d54
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-15 09:45:19 +0100

    FIX: Resolved request and response issue with NextResponse and NextRequest

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,5 +1,5 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { NextRequest, NextResponse } from "next/server";
+import validator from "validator";
 
 export async function GET(req: NextRequest) {
   return NextResponse.json({
@@ -8,10 +8,57 @@ export async function GET(req: NextRequest) {
   });
 }
 
-export async function POST(req: NextRequest) {
+export async function POST(request: NextRequest, response: NextResponse) {
+  const { firstName, lastName, email, phone, city, password } =
+    await request.json();
+  const errors: string[] = [];
+  const validationSchema = [
+    {
+      valid: validator.isLength(firstName, {
+        min: 1,
+        max: 20,
+      }),
+      errorMessage: "First name is invalid",
+    },
+    {
+      valid: validator.isLength(lastName, {
+        min: 1,
+        max: 20,
+      }),
+      errorMessage: "Last name is invalid",
+    },
+    {
+      valid: validator.isEmail(email),
+      errorMessage: "Email is invalid",
+    },
+    {
+      valid: validator.isMobilePhone(phone),
+      errorMessage: "Phone number is invalid",
+    },
+    {
+      valid: validator.isLength(city, { min: 1 }),
+      errorMessage: "City is invalid",
+    },
+    {
+      valid: validator.isStrongPassword(password),
+      errorMessage: "Password is invalid",
+    },
+  ];
+  validationSchema.forEach((check) => {
+    if (!check.valid) {
+      errors.push(check.errorMessage);
+    }
+  });
+  if (errors.length) {
+    return NextResponse.json({ errorMessage: errors[0] }, { status: 400 });
+  }
   return NextResponse.json({
-    name: "Nextjs POST",
-    route: "api/auth/signup",
+    firstName,
+    lastName,
+    email,
+    phone,
+    city,
+    password,
   });
 }
 
