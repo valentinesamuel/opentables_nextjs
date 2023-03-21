be433375f361d5d8b3461624468544649ae2e0c2
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-21 11:11:23 +0000

    FEAT: Added search times

--- app/api/restaurant/[slug]/availability/route.ts
+++ app/api/restaurant/[slug]/availability/route.ts
@@ -1,3 +1,4 @@
+import { times } from "@/data";
 import { getQueryParams } from "@/utils/getQueryParams";
 import { PrismaClient } from "@prisma/client";
 import { NextRequest, NextResponse } from "next/server";
@@ -21,6 +22,17 @@ export async function GET(request: NextRequest, context: { params: any }) {
   //       slug: "whatevs";
   //     }
   //   }
+  const searchTimes = times.find((t) => {
+    return t.time === time;
+  })?.searchTimes;
+  if (!searchTimes) {
+    return NextResponse.json(
+      {
+        errorMessage: "Invalid data provided",
+      },
+      { status: 400 }
+    );
+  }
 
-  return NextResponse.json({ slug, day, time, partySize });
+  return NextResponse.json({ searchTimes });
 }
