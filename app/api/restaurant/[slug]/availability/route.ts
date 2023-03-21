28c7cced5ed2c72fc74f667f5a5caf1870d600ea
valentinesamuel <valentinesamuel2580@gmail.com>
2023-03-21 14:10:48 +0000

    FEAT: Great course but very few broken code due to the usage of the beta version

--- app/api/restaurant/[slug]/availability/route.ts
+++ app/api/restaurant/[slug]/availability/route.ts
@@ -6,7 +6,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 const prisma = new PrismaClient();
 
-export async function GET(request: NextRequest, context: { params: any }) {
+export async function GET(request: NextRequest) {
   const { slug, day, time, partySize } = getQueryParams(request.url);
   console.log({ slug, day, time, partySize });
 
